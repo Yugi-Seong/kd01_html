3ca3c071dbb39c112777c42447c886366f163f32
Yugi-Seong <tjddbrl64@gmail.com>
2025-02-20 19:40:27 +0900

    폭탄게임수정완료 및  박스오피스 API

--- 12/12.js
+++ 12/12.js
@@ -1,65 +1,82 @@
 let arr = [0,0,0,0,0,0,0,0,1] ; // 1 위치가 폭탄 위치 => 배열의 위치를 변경 : 셔플
-let isFlag = true ;
+let isFlag = false ;
 console.log(arr);
 let number ;
 
+// 리셋 함수 생성
+const init = (cols) => {
+    cnt = 0;
+    isFlag = true ;
+
+    // 보드에 숫자쓰기 
+    for(let [idx,col] of cols.entries()){
+        col.innerHTML =  idx + 1;
+    };
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
+    // 제어할 요소 가져오기 
     const cols = document.querySelectorAll('.col');
     const btn = document.querySelector('button');
+    const msg = document.querySelector('#msg');
 
-    // // 보드에 숫자 쓰기 
-    // for(let [idx,col] of cols.entries()){
-    //     col.innerHTML = idx + 1;
-    // }
+    // 하트갯수 
+    let cnt = 0 ; 
 
     // 클릭시 해당 인덱스 출력 
     for(let [idx,col] of cols.entries()){
         col.addEventListener('click',() =>{
-            if ( isFlag ){
-                console.log(idx+1);
-           
-                // 폭탄의 번호를 클릭할 경우 게임 종료
-                if (idx+1 == number){
-                    col.innerHTML = '<img src="../img/boom.png">' ;
-                    msg.innerHTML = "게임이 종료 되었습니다.";
-                
-                    isFlag = false ;
-                    
 
-                }else {
-                    // 폭탄이 아닌 버튼 클릭시 하트가 나오게
-                col.innerHTML = '<img src="../img/hart.png">' ;
-
-                }
-                // 게임 중에 번호 나오지 않도록 
-                btn.style.display ='none' ;
-
+            if ( !isFlag ){
+                cnt = 0 ;
+                msg.innerHTML = "폭탄을 섞어주세요." ;
+                btn.innerHTML = "폭탄 섞기";
+                return ;
             }
+            
+            // 폭탄의 번호를 클릭할 경우 게임 종료
+            if (idx+1 == number){
+                col.innerHTML = '<img src="../img/boom.png">' ;
+                msg.innerHTML = "게임이 종료 되었습니다.";
+                btn.innerHTML = "다시하기";
+                
+                btn.disabled =false ; // 버튼 활성화 
+                
+                isFlag = false ; // 더이상 안 눌러지게 설정 
+                return ;
+                }
+            // 게임 중
+            if (idx+1 != number){
+                // 게임 중에 버튼 비활성화 
+                btn.disabled =true ;
+                btn.innerHTML = "게임중...";
+                // 폭탄이 아닌 버튼 클릭시 하트가 나오게
+                col.innerHTML = '<img src="../img/hart.png">' ;
+                cnt++;
+                
+                // 하트 8개가 나온 후 나머지 것은 폭탄을 하트로 변경 후 성공
+                if (cnt == 8){
+                    // 1이 있는 위치에 하트 추가 
+                    cols[number-1].innerHTML = '<img src="../img/hart.png">';
+                    msg.innerHTML = "성공" ;
+                    btn.innerHTML = "다시하기";
+                    btn.disabled =false ;
+                    init(col); 
+                    return;
+                    }
+                } 
         });
-    }
+    } 
 
 
-    // 폭탄 섞이 버튼 눌릴경우 폭탄섞기
+    
     btn.addEventListener('click',()=>{
-        // // 배열 셔플 
-        // arr.sort(() => Math.random()-0.5);
-        // console.log(Math.random()-0.5)
-        // console.log(arr);
-
+        cnt = 0; 
         // 폭탄 번호 생성 
         number = Math.floor(Math.random() * 9)+1 ;
         console.log("폭탄 번호 : "+number);
+        msg.innerHTML = "";
 
-        for(let [idx,col] of cols.entries()){
-            col.innerHTML =  idx + 1;
-        };
-         
+        init(cols);
     });
-
-
-    
-    
-
-    // 하트 8개가 나온 후 나머지 것은 폭탄을 하트로 변경 후 성공 
-
 });
---
+++ 13/13.js
@@ -0,0 +1,48 @@
+
+const getFetch = async (dt,ul) =>{
+    console.log(dt)
+
+    let apiKey = '' ;
+    let baseUrl = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
+    let url = `${baseUrl}key=${apiKey}&targetDt=${dt}` ;
+
+    // fetch API 
+    try{
+        const resp = await fetch(url);
+        const data = await resp.json();
+        console.log(data.boxOfficeResult.dailyBoxOfficeList)
+        
+        let tags = '';
+
+        for(let item of data.boxOfficeResult.dailyBoxOfficeList){
+            
+            tags = tags + 
+            `<li>
+                <span class="sp">${item.rank}</span>
+                ${item.movieNm}
+            </li>` ;
+        }
+        ul.innerHTML = tags ;
+        
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    // 필요한 요소 가지고 오기 
+    const dt = document.querySelector('#dt');
+    let ul = document.querySelector('ul');
+
+
+    dt.addEventListener('change',(e)=>{
+        e.preventDefault ;
+        console.log(dt.value);
+        getFetch(dt.value.replaceAll('-',''),ul) ;
+        
+        
+    });
+
+});
